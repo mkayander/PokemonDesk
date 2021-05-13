e86f5b066ea5375b8e474d353936251c9523c1cf
MaxKayan <maxim.kayander1@gmail.com>
2021-05-13 13:41:54 +0300

    Added HomeTask

--- src/example.ts
+++ src/example.ts
@@ -1,3 +1,22 @@
+function concat(first: string, second: string) {
+    return first + second
+}
+
+console.log(concat("Hello", " world!"));
+
+type HomeTask = {
+    howIDoIt: string
+    tripleArray: [string, number] | [string, string, number]
+    withData?: Array<HomeTask>
+}
+
+const myHomeTask: HomeTask = {
+    howIDoIt: "I Do It Wel",
+    tripleArray: ["string one", "string two", 42],
+    withData: [{ howIDoIt: "I Do It Wel", tripleArray: ["string one", 23] }],
+}
+console.log(myHomeTask);
+
 interface MyArray<T> {
     length: number
 
