db2f625ce9408e2bebcd5a5fb73cf47213eea458
MaxKayan <maxim.kayander1@gmail.com>
2021-05-13 15:59:56 +0300

    Formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,35 +2,36 @@ module.exports = {
     env: {
         browser: true,
     },
-    extends: ['airbnb', 'airbnb/hooks', 'react-app', 'prettier', 'prettier/react'],
-    parser: '@typescript-eslint/parser',
+    extends: ["airbnb", "airbnb/hooks", "react-app", "prettier", "prettier/react"],
+    parser: "@typescript-eslint/parser",
     parserOptions: {
         ecmaFeatures: {
             jsx: true,
         },
         ecmaVersion: 12,
-        sourceType: 'module',
+        sourceType: "module",
     },
-    plugins: ['prettier', 'react', '@typescript-eslint'],
+    plugins: ["prettier", "react", "@typescript-eslint"],
     rules: {
-        '@typescript-eslint/no-unused-vars': 'warn',
-        'no-console': 'warn',
-        'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx', '.ts', '.js'] }],
-        'import/extensions': [
-            'error',
-            'ignorePackages',
+        "@typescript-eslint/no-unused-vars": "warn",
+        "no-console": "off",
+        "react/jsx-filename-extension": ["error", { extensions: [".jsx", ".tsx", ".ts", ".js"] }],
+        "import/extensions": [
+            "error",
+            "ignorePackages",
             {
-                ts: 'never',
-                tsx: 'never',
-                js: 'never',
-                jsx: 'never',
+                ts: "never",
+                tsx: "never",
+                js: "never",
+                jsx: "never",
             },
         ],
+        // "import/newline-after-import": ["warn", { count: 1 }],
     },
     settings: {
-        'import/resolver': {
+        "import/resolver": {
             webpack: {
-                config: 'webpack.config.js',
+                config: "webpack.config.js",
             },
         },
     },
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-
+import React from "react";
+import classNames from "classnames";
 import "./custom.css";
 import styles from "./App.module.scss";
 
-import classNames from "classnames";
-
 const App = () => {
     console.log(process.env.NODE_ENV);
+    console.log("$@#### TEST");
+
     return (
         <div>
-            <h3 className={classNames(styles.header, "color-red")}>
-                This is App Component!
-            </h3>
+            <h3 className={classNames(styles.header, "color-red")}>This is App Component!</h3>
             <div className={styles.main}>
-                <p>
-                    Welcome my friend
-                </p>
-                <p>
-                    This is some lorem ipsum text
-                </p>
+                <p>Welcome my friend</p>
+                <p>This is some lorem ipsum text</p>
             </div>
         </div>
     );
