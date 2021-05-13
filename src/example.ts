beaa137f4efd3b453da0ab4e638425df4fdd27d6
MaxKayan <maxim.kayander1@gmail.com>
2021-05-13 10:06:10 +0300

    Created my own Array interface for testing

---
+++ src/example.ts
@@ -0,0 +1,21 @@
+interface MyArray<T> {
+    length: number
+
+    [n: number]: T
+
+    pop(): T | undefined;
+
+    map<U>(fn: (element: T) => U): U[]
+
+    reduce<N>(fn: (acc: N, element: T) => N, initial: N): N
+}
+
+const myArr: MyArray<string> = ["a", "b", "c", "d"];
+
+console.log(myArr.map(element => element + 22));
+
+console.log(myArr.reduce((acc, element) => {
+    return +acc + +element;
+}, 1));
+
+const array: Array<number> = [];
