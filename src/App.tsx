3af3e3dfe2f5736f14d92cd718dd5701771be0ed
Max Kayander <maxim.kayander1@gmail.com>
2021-05-16 00:32:35 +0300

    Added home page component. Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import React from "react";
-import classNames from "classnames";
 import "./custom.css";
-import styles from "./App.module.scss";
+import HomePage from "./pages/Home";
 
 const App = () => {
-    console.log(process.env.NODE_ENV);
-    console.log("$@#### TEST");
 
     return (
-        <div>
-            <h3 className={classNames(styles.header, "color-red")}>This is App Component!</h3>
-            <div className={styles.main}>
-                <p>Welcome my friend</p>
-                <p>This is some lorem ipsum text</p>
-            </div>
-        </div>
+        <HomePage/>
     );
 };
 
