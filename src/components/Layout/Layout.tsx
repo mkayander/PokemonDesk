8441d463d31f213e2305df8d694b0361a25650f9
Max Kayander <maxim.kayander1@gmail.com>
2021-05-16 01:21:15 +0300

    Fixed Layout.tsx prop types

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,7 +4,7 @@ import cn from "classnames";
 import styles from "./Layout.module.scss";
 
 // eslint-disable-next-line react/prop-types
-const Layout: React.FC<HTMLDivElement> = ({ children, className = null }) => (
+const Layout: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({ children, className = null }) => (
     <div className={cn(styles.root, className)}>{children}</div>
 );
 
