9290fbd5c03c1e5894aeff5f19deb3bd76210b2a
Max Kayander <maxim.kayander1@gmail.com>
2021-05-16 20:18:34 +0300

    Finished Button component. Added global bg-color and heading styles.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,17 +1,45 @@
-import React from 'react';
+import React from "react";
+import cn from "classnames";
 
-import styles from "./Button.module.scss"
+import styles from "./Button.module.scss";
+import { BgColor } from "../../index";
 
-type ButtonProps = {
-    text: string
+// eslint-disable-next-line no-shadow
+export enum BtnSize {
+    normal,
+    small,
 }
 
-const Button: React.FC<React.HTMLAttributes<HTMLButtonElement> & ButtonProps> = ({text, ...rest}) => {
+type ButtonProps = {
+    text?: string;
+    fullWidth?: boolean;
+    bgColor?: BgColor;
+    btnSize?: BtnSize;
+};
+
+const Button: React.FC<React.HTMLAttributes<HTMLButtonElement> & ButtonProps> = ({
+    text,
+    bgColor = "bg-confirm",
+    btnSize = BtnSize.normal,
+    fullWidth = false,
+    children,
+    ...rest
+}) => {
     return (
-        <button type="button" className={styles.root} {...rest}>
-            <span>
-                {text}
-            </span>
+        <button
+            type="button"
+            className={cn(
+                styles.root,
+                {
+                    [styles.fullWidth]: fullWidth,
+                    [styles.small]: btnSize === BtnSize.small,
+                },
+                styles[bgColor],
+                bgColor
+            )}
+            {...rest}>
+            {text && <span>{text}</span>}
+            {children}
         </button>
     );
 };
--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,6 @@ import App from "./App";
 
 import './index.scss';
 
+export type BgColor = "bg-primary" | "bg-secondary" | "bg-confirm" | "bg-danger" | "bg-warning";
+
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
