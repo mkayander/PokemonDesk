d317085111bf25cd8a7282be994e0e13533440ed
Max Kayander <maxim.kayander1@gmail.com>
2021-05-16 20:19:02 +0300

    Added Heading.tsx component. Adjusted HomePage.tsx.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 // eslint-disable-next-line import/prefer-default-export
 export {default as Header} from "./Header/Header"
 export {default as Button} from "./Button/Button"
+export {default as HomeParallax} from "./HomeParallax/HomeParallax"
+export {default as Heading} from "./Heading/Heading"
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 
+import classNames from "classnames";
 import styles from "./HomePage.module.scss";
-import { Button, Header } from "../../components";
+import { Button, Header, Heading, HomeParallax } from "../../components";
 import Layout from "../../components/Layout/Layout";
+import { BtnSize } from "../../components/Button/Button";
 
 const HomePage = () => {
     return (
@@ -10,11 +12,20 @@ const HomePage = () => {
             <Header />
             <Layout className={styles.main}>
                 <div className={styles.column}>
-                    <h1 className={styles.heading}>Find all your favorite Pokemon</h1>
-                    <h3 className={styles.subtitle}>
+                    <Heading variant="h1">
+                        Find all your favorite Pokemon
+                    </Heading>
+                    <Heading>
                         You can know the type of Pokemon, its strengths, disadvantages and abilities
-                    </h3>
+                    </Heading>
+                    <Button text="Start now!" fullWidth bgColor="bg-warning" />
                     <Button text="See pokemons" onClick={event => console.log(event)} />
+                    <Button
+                        text="Read more..."
+                        onClick={event => console.log(event)}
+                        bgColor="bg-secondary"
+                        btnSize={BtnSize.small}
+                    />
                 </div>
                 <div className={classNames(styles.column, styles.contentRight)}>
                     <HomeParallax />
