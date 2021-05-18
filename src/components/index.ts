9ef52df0516be03720868cf52dfceb417b9400a1
MaxKayan <maxim.kayander1@gmail.com>
2021-05-18 10:45:53 +0300

    Added layout export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
-export {default as Header} from "./Header/Header"
+export { default as Layout } from "./Layout/Layout";
+export { default as Header } from "./Header/Header";
 export {default as Button} from "./Button/Button"
 export {default as HomeParallax} from "./HomeParallax/HomeParallax"
 export {default as Heading} from "./Heading/Heading"
