c24f0a9902bf3fd6ac94b2a63debdeefd6825d01
Max Kayander <maxim.kayander1@gmail.com>
2021-05-18 23:53:14 +0300

    Added required styles to the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import React from "react";
 import "./custom.css";
 import { useRoutes } from "@patched/hookrouter";
 import routes from "./routes";
+import { NotFoundPage } from "./pages";
+
+import styles from "./App.module.scss"
 
 const App = () => {
-    return useRoutes(routes);
+    return (
+        <div className={styles.root}>
+            {useRoutes(routes) || <NotFoundPage />}
+        </div>
+    )
 };
 
 export default App;
