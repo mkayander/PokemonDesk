72b7ea7e79640b7e78100a4f2eadf9f67dcd2f07
Max Kayander <maxim.kayander1@gmail.com>
2021-05-18 23:54:56 +0300

    Added Pokedex page, Card component, mock data

---
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import s from "./PokemonCard.module.scss";
+import { Heading } from "../index";
+
+const PokemonCard = () => {
+    return (
+        <div className={s.root}>
+            <div className={s.infoWrap}>
+                <Heading className={s.titleName}>Charmander</Heading>
+                <div className={s.statWrap}>
+                    <div className={s.statItem}>
+                        <div className={s.statValue}>52</div>
+                        Attack
+                    </div>
+                    <div className={s.statItem}>
+                        <div className={s.statValue}>43</div>
+                        Defense
+                    </div>
+                </div>
+                <div className={s.labelWrap}>
+                    <span className={s.label}>Fire</span>
+                </div>
+            </div>
+            <div className={s.pictureWrap}>
+                <img
+                    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"
+                    alt="Charmander"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default PokemonCard;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,5 @@
 // eslint-disable-next-line import/prefer-default-export
 export {default as HomePage} from "./Home/HomePage";
 export {default as EmptyPage} from "./Empty/EmptyPage";
+export {default as NotFoundPage} from "./NotFound/NotFoundPage";
+export {default as PokedexPage} from "./PokedexPage/PokedexPage";
