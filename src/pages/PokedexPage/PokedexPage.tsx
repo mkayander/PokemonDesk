8fb5d16e07c98686f8bb17d0d3aa9e08d48e40cc
MaxKayan <maxim.kayander1@gmail.com>
2021-05-19 16:00:45 +0300

    Implemented Pokedex Page.

--- src/pages/PokedexPage/PokedexPage.tsx
+++ src/pages/PokedexPage/PokedexPage.tsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { ContentPageBase, PokemonCard } from "../../components";
+
+import styles from "./PokedexPage.module.scss";
+import { getPokemons } from "../../data/api/api";
 
 const PokedexPage = () => {
     return (
-        <div>
+        <ContentPageBase className={styles.root} bgColor="bg-grey-gradient">
+            <h1 className={styles.headText}>
+                800 <b>Pokemons</b> for you to choose your favorite
+            </h1>
+            <div className={styles.searchInput}>
+                <input placeholder="Enter pokemon name..." />
+            </div>
 
-        </div>
+            <div className={styles.cardsContainer}>
+                {getPokemons().map(pokemon => (
+                    <PokemonCard key={pokemon.id} pokemon={pokemon} />
+                ))}
+            </div>
+        </ContentPageBase>
     );
 };
 
