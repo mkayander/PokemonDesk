c2c9b7f8c1bf6786d2de4300513f59d29010791e
MaxKayan <maxim.kayander1@gmail.com>
2021-05-19 15:53:56 +0300

    Optimized Heading.tsx component. Fixed className prop not taking effect.

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,27 +1,17 @@
-import React, { HTMLAttributes, useMemo } from "react";
+import React, { HTMLAttributes } from "react";
 
 type HeadingProps = {
     variant?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 };
 
-const Heading: React.FC<HTMLAttributes<HTMLHeadingElement> & HeadingProps> = ({ variant, children }) => {
-    return useMemo(() => {
-        switch (variant) {
-            case "h1":
-                return <h1>{children}</h1>;
-            case "h2":
-                return <h2>{children}</h2>;
-            default:
-            case "h3":
-                return <h3>{children}</h3>;
-            case "h4":
-                return <h4>{children}</h4>;
-            case "h5":
-                return <h5>{children}</h5>;
-            case "h6":
-                return <h6>{children}</h6>;
-        }
-    }, [variant, children]);
+const Heading: React.FC<HTMLAttributes<HTMLHeadingElement> & HeadingProps> = ({
+    variant = "h3",
+    children,
+    className,
+}) => {
+    const Tag = variant as keyof JSX.IntrinsicElements;
+
+    return <Tag className={className}>{children}</Tag>;
 };
 
 export default Heading;
