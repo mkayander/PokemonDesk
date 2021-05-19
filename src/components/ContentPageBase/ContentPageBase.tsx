62d42962231501e33dad5b4a31ae44f190a15421
MaxKayan <maxim.kayander1@gmail.com>
2021-05-19 15:55:44 +0300

    Added new ContentPageBase.tsx component to lower the amount of repeating code when making page components.

---
+++ src/components/ContentPageBase/ContentPageBase.tsx
@@ -0,0 +1,30 @@
+import React, { HTMLAttributes } from "react";
+import cn from "classnames";
+import { Header, Layout } from "../index";
+
+import styles from "./ContentWrap.module.scss";
+import { BgColor } from "../../index";
+
+type ContentWrapProps = {
+    useHeader?: boolean;
+    bgColor?: BgColor;
+};
+
+const ContentPageBase: React.FC<HTMLAttributes<HTMLDivElement> & ContentWrapProps> = ({
+    useHeader = true,
+    bgColor = "bg-primary-gradient",
+    children,
+    className,
+    ...rest
+}) => {
+    return (
+        <div className={cn(styles.root, bgColor)}>
+            {useHeader && <Header />}
+            <main {...rest}>
+                <Layout className={cn(styles.content, className)}>{children}</Layout>
+            </main>
+        </div>
+    );
+};
+
+export default ContentPageBase;
