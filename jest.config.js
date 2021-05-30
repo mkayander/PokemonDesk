ea0e3dd6bde008e5ede823f176f2f692ebae9754
Max Kayander <maxim.kayander1@gmail.com>
2021-05-30 22:05:23 +0300

    Improved Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+    preset: "ts-jest",
+    testEnvironment: "node",
+    moduleNameMapper: {
+        "^.+\\.(css|less|scss|sass)$": "identity-obj-proxy",
+        "^.+\\.(svg|png)$": "identity-obj-proxy",
+    },
 };
